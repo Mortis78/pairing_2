9dce012de3e4283879da2c79fc672f5ddf9b9001
Johnathon Ammon <johnathonammon@Johnathons-MacBook-Pro.local>
2022-09-21 14:51:46 -0600

    Add second array

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var arrays = ["pizza", "carrot", "sushi", "pie", "bread"]
 console.log(arrays[2])
 console.log(arrays[4])
 console.log(arrays[0])
+
+var places = ["paris", "callifornia", "north carolina", "the zoo", "the park"]
+
+console.log(places.pop())
